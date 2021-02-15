47c5ceac61358464399156ffcd7daf52d2ffe943
fchoi0505 <73865311+fchoi0505@users.noreply.github.com>
2021-02-14 17:57:50 -0800

    Update logic.js

--- Mapping_Multi_Points/static/js/logic.js
+++ Mapping_Multi_Points/static/js/logic.js
@@ -11,8 +11,8 @@ let cityData = cities;
 cityData.forEach(function(city) {
     console.log(city)
     L.circleMarker(city.location, {
-        radius: city.population/100000
-        color: "orange"
+        radius: city.population/100000,
+        color: "orange",
         fillColor: 'orange'
     })
     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
